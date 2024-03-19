8e1bab88e433ffc6fad83212c437152364f626f1
Ingrid G <ing.gape18@gmail.com>
2024-03-18 20:43:03 -0500

    component import ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaAutosComponent } from './autos/lista-autos/lista-autos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    ListaAutosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/autos/lista-autos/lista-autos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lista-autos',
+  templateUrl: './lista-autos.component.html',
+  //styleUrl: './app.component.css'
+})
+export class ListaAutosComponent {
+  title = 'lista-autos';
+}
